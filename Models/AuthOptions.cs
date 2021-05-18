40a958431a95e1cd240979baa5a94caa1bd1d82f
Marcelo <marcaldo@gmail.com>
2021-05-17 20:57:37 -0500

    Key Api local storage

---
+++ Models/AuthOptions.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BiDegree.Models
+{
+    public class AuthOptions
+    {
+        public string DriveApiKey { get; set; }
+    }
+}
