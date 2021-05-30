59c45258415b514e7d6135704cf03e8f7fb17e95
marcaldo <marcaldo@gmail.com>
2021-05-30 21:50:26 +0000

    Deploying to gh-pages from  @ 9b471f82c28fd071057888e5bc3f7a8fe80cdaaf 🚀

--- service-worker-assets.js
+++ service-worker-assets.js
@@ -265,7 +265,7 @@
       "url": "_framework\/System.IO.Pipelines.dll"
     },
     {
-      "hash": "sha256-GSsDa16nmUpoAkAF+yNwuo2y45hq15zzHSlsXLMXeh4=",
+      "hash": "sha256-0Qn+vp5oJg7D\/GqGABOjG1d3IBU1D6KgnfOzK\/d\/uMU=",
       "url": "_framework\/BiDegree.dll"
     },
     {
@@ -369,7 +369,7 @@
       "url": "_framework\/System.Private.CoreLib.dll"
     },
     {
-      "hash": "sha256-jrPZd3C28hWoL9Ik6I4iwmTMEvdLSjmGW0q1nguRpkI=",
+      "hash": "sha256-n8QW4a3OctqVyvMUEcZ2PojCODiD2nVQM+sVtv+dRBY=",
       "url": "_framework\/blazor.boot.json"
     },
     {
@@ -377,5 +377,5 @@
       "url": "_framework\/blazor.webassembly.js"
     }
   ],
-  "version": "EzgjnADm"
+  "version": "rGHiqIkF"
 };
--- service-worker.js
+++ service-worker.js
@@ -46,4 +46,4 @@ async function onFetch(event) {
 
     return cachedResponse || fetch(event.request);
 }
-/* Updated 05/16/2021 03:59 pm *//* Manifest version: EzgjnADm */
+/* Updated 05/16/2021 03:59 pm *//* Manifest version: rGHiqIkF */
