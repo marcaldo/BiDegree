a6ed79de587229d277fd4d8fecb81e705d4aa94a
Marcelo <marcaldo@gmail.com>
2021-06-04 15:35:27 -0500

    Changes in the clock

--- Shared/Clock.razor.cs
+++ Shared/Clock.razor.cs
@@ -1,12 +1,19 @@
-﻿using System;
+﻿using Blazored.LocalStorage;
+using Microsoft.AspNetCore.Components;
+using System;
+using System.Threading.Tasks;
 using System.Timers;
 
 namespace BiDegree.Shared
 {
     public partial class Clock
     {
-        private string DisplayDate { get; set; }
-        private string DisplayTime { get; set; }
+        [Inject] ILocalStorageService localStorage { get; set; }
+
+        private string DisplayDate;
+        private string DisplayTime;
+        private string AmPm;
+        private TimeFormatType timeFormat = TimeFormatType.T12hs;
         public Clock()
         {
             Timer timer = new();
@@ -15,6 +22,11 @@ namespace BiDegree.Shared
             timer.Enabled = true;
 
             SetDateTime();
+        }
+
+        protected override async Task OnInitializedAsync()
+        {
+            timeFormat = await localStorage.GetItemAsync<TimeFormatType?>(Constants.TimeFormat) ?? TimeFormatType.T12hs;
         }
 
         private void Timer_Elapsed(object _, ElapsedEventArgs e)
@@ -26,8 +38,20 @@ namespace BiDegree.Shared
         private void SetDateTime()
         {
             DateTime now = DateTime.Now;
-            DisplayDate = $"{now.DayOfWeek.ToString()[..3].ToUpper()}, {now.ToString("MMM")} {now:dd}, '{now:yy}";            
-            DisplayTime = now.ToString("t");
+            DisplayDate = $"{now.DayOfWeek.ToString()[..3].ToUpper()}, {now:MMM} {now:dd}, '{now:yy}";
+
+            Console.WriteLine(timeFormat);
+
+            if (timeFormat == TimeFormatType.T12hs)
+            {
+                DisplayTime = now.ToString("h:mm");
+                AmPm = now.ToString("tt");
+                return;
+            }
+            
+            DisplayTime = now.ToString("HH:mm");
+            AmPm = "";
         }
     }
 }
+
