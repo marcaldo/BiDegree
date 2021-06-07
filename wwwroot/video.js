79076991c04a95923d051e661f990cd4ac9068c9
Marcelo <marcaldo@gmail.com>
2021-06-07 10:42:59 -0500

    blazored video tests

---
+++ wwwroot/video.js
@@ -0,0 +1,11 @@
+﻿var video = document.getElementsByTagName("video");
+
+
+//function playVideo() {
+//    video[0].play();
+//}
+
+function playVideo()
+{
+    setTimeout(function () { video[0].play(); }, 1000);
+};
