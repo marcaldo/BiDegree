bdf06bcb536415c585ac96bb721920d83a549ef9
marcaldo <marcaldo@gmail.com>
2021-06-27 15:52:21 +0000

    Deploying to gh-pages from  @ d43882595f6649fe8d8b041f43eb805a2d84ae33 🚀

--- service-worker.js
+++ service-worker.js
@@ -46,4 +46,4 @@ async function onFetch(event) {
 
     return cachedResponse || fetch(event.request);
 }
-/* Updated 05/16/2021 03:59 pm *//* Manifest version: HF6fvZGP */
+/* Updated 05/16/2021 03:59 pm *//* Manifest version: U7cvhmyL */
