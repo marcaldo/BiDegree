6df7c29708be801173291f33ed9d1531881dbbda
marcaldo <marcaldo@gmail.com>
2022-02-10 12:59:05 -0600

    Version update

--- wwwroot/service-worker.published.js
+++ wwwroot/service-worker.published.js
@@ -46,4 +46,4 @@ async function onFetch(event) {
 
     return cachedResponse || fetch(event.request);
 }
-/* >>> Build updated Friday, January 21, 2022 17:49:20 <<< */
+/* >>> Build updated Thursday, February 10, 2022 12:58:55 <<< */
