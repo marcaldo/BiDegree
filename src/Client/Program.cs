487f7156b55aa6bc6aa32297d2225decb75a9cb9
marcaldo <marcaldo@gmail.com>
2024-01-16 17:39:31 -0600

    wip

--- src/Client/Program.cs
+++ src/Client/Program.cs
@@ -18,11 +18,15 @@ namespace BiDegree
         {
 
             var builder = WebAssemblyHostBuilder.CreateDefault(args);
-            builder.RootComponents.Add<App>("#app");
-
-            builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.Configuration["BiDegree.API.BaseAddress"]) });
 
             builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+            builder.Services.AddScoped(sp => sp.GetRequiredService<IHttpClientFactory>().CreateClient("BiDegree.API"));
+
+            builder.Services.AddHttpClient();
+
+            builder.RootComponents.Add<App>("#app");
+
             builder.Services.AddScoped<IWeatherApi, OpenWeatherApi>();
             builder.Services.AddScoped<IGoogleDriveApi, GoogleDriveAPI>();
             builder.Services.AddScoped<IDebugMode, DebugMode>();
